143e79e1410ffb62ad785c13854e2631b5d2d2af
George Popovic <georgesmpopovic@gmail.com>
2022-08-12 15:29:29 -0400

    reformat file with indentation

--- src/MediumApi/MediumApi.php
+++ src/MediumApi/MediumApi.php
@@ -30,10 +30,6 @@ class MediumApi
 
     public $request = null; 
 
-    public $response = null;
-
-    public $err = null;
-
     public function __construct()
     {
 
@@ -46,192 +42,201 @@ class MediumApi
 
     }
 
-    /* Set the HTTP headers that authorize whether we can access the 3rd party API on Rapid API. */
-    public function setRapidApiHeaders(){
 
-        $this->request->headers["X-RapidAPI-Host"] = "X-RapidAPI-Host: " . $this->rapid_api_medium_api_host;
-        $this->request->headers["X-RapidAPI-Key"] = "X-RapidAPI-Key: " . $this->rapid_api_medium_api_key;
+    /* ********************************************************* */
+    /* Medium Rapid API functions */
+    /* ********************************************************* */
+
+        /* Set the HTTP headers that authorize whether we can access the 3rd party API on Rapid API. */
+        public function setRapidApiHeaders(){
+
+            $this->request->headers["X-RapidAPI-Host"] = "X-RapidAPI-Host: " . $this->rapid_api_medium_api_host;
+            $this->request->headers["X-RapidAPI-Key"] = "X-RapidAPI-Key: " . $this->rapid_api_medium_api_key;
+        
+        }
+        /* Erase the HTTP headers that authorize whether we can access the 3rd party API on Rapid API. */
+        public function unsetRapidApiHeaders(){
+
+            unset($this->request->headers["X-RapidAPI-Host"]);
+            unset($this->request->headers["X-RapidAPI-Key"]);
+        
+        }
+
+        /* Get the user's ID associated with their username. */
+        public function getUserId()
+        {
+
+            $url = "https://medium2.p.rapidapi.com/user/id_for/" . $this->user_name;
+
+            $this->request->get($url);
+
+            $api_response = json_decode($this->request->response, true);
+
+            if (isset($api_response["id"])) {
+
+                $this->user_id = $api_response["id"];
+
+            }
+
+        }
+
+        /* ********************************************************* */
+        /* Functions related to etrieving Articles */
+        /* ********************************************************* */
+
+        /* Getting a user's articles, their info, and their contents */
+        public function getUserArticlesIds()
+        {
+            $url = "https://medium2.p.rapidapi.com/user/" . $this->user_id . "/articles";
+
+            $this->request->get($url);
+
+            $api_response = json_decode($this->request->response, true);
+
+            if (isset($api_response["associated_articles"])) {
+
+                $this->associated_articles_ids = $api_response["associated_articles"];
+
+            }
+
+        }
+
+        /* Get the content (non-html/non-markdown) for a particular article. */
+        public function getUserArticleContent($associated_article_id)
+        {
+
+            $url = "https://medium2.p.rapidapi.com/article/" . $associated_article_id . "/content";
+
+            $this->request->get($url);
+
+            $api_response = json_decode($this->request->response, true);
+
+            if (isset($api_response["content"])) {
+
+                return $api_response["content"];
+
+            }
+
+        }
+
+        /* Get the content (non-html/non-markdown) for all user's articles associated with our object. */
+        public function getUserArticlesContents()
+        {
+
+            foreach ($this->associated_articles_ids as $associated_article_id) {
+
+                $content = $this->getUserArticleContent($associated_article_id);
+
+                $this->associated_articles_contents[] = [
+                    "article_id" => $associated_article_id,
+                    "content" => $content,
+                ];
+
+            }
+
+        }
+
+        /* Get the content (markdown) for a particular article. */
+        public function getUserArticleMarkdown($associated_article_id)
+        {
+
+            $url = "https://medium2.p.rapidapi.com/article/" . $associated_article_id . "/markdown";
+
+            $this->request->get($url);
+
+            $api_response = json_decode($this->request->response, true);
+
+            if (isset($api_response["markdown"])) {
+
+                return $api_response["markdown"];
+
+            }
+
+        }
+
+        /* Get the content (markdown) for all user's articles associated with our object. */
+        public function getUserArticlesMarkdowns()
+        {
+
+            foreach ($this->associated_articles_ids as $associated_article_id) {
+
+                $markdown = $this->getUserArticleMarkdown($associated_article_id);
+
+                $this->associated_articles_markdowns[] = [
+                    "article_id" => $associated_article_id,
+                    "markdown" => $markdown,
+                ];
+
+            }
+
+        }
+
+        /* Get the info/metadata for a particular article. */
+        public function getUserArticleInfo($associated_article_id)
+        {
+
+            $url = "https://medium2.p.rapidapi.com/article/" . $associated_article_id;
+
+            $this->request->get($url);
+
+            $api_response = json_decode($this->request->response, true);
+
+            return $api_response;
+
+        }
+
+        /* Get the info/metadata for all user's articles associated with our object. */
+        public function getUserArticlesInfos()
+        {
+
+            foreach ($this->associated_articles_ids as $associated_article_id) {
+
+                $info = $this->getUserArticleInfo($associated_article_id);
+
+                $this->associated_articles_infos[] = [
+                    "article_id" => $associated_article_id,
+                    "info" => $info,
+                ];
+
+            }
+
+        }
+
+        /* Get the content, markdown, and metadata for all of our user's article associated with our object,
+        and file it away nicely, in memory. 
+        */
+        public function getUserArticles()
+        {
+
+            $this->getUserId();
+
+            $this->getUserArticlesIds();
+
+            foreach ($this->associated_articles_ids as $associated_article_id) {
+
+                $info = $this->getUserArticleInfo($associated_article_id);
+                $markdown = $this->getUserArticleMarkdown($associated_article_id);
+                $content = $this->getUserArticleContent($associated_article_id);
+
+                $this->associated_articles[] = [
+                    "article_id" => $associated_article_id,
+                    "info" => $info,
+                    "markdown" => $markdown,
+                    "content" => $content
+                ];
+
+            }
+
+        }
+
+        /* ********************************************************* */
+        /* Functions related to etrieving Articles */
+        /* ********************************************************* */
+
+    /* ********************************************************* */
+    /* Medium Rapid API functions */
+    /* ********************************************************* */
     
-    }
-    /* Erase the HTTP headers that authorize whether we can access the 3rd party API on Rapid API. */
-    public function unsetRapidApiHeaders(){
-
-        unset($this->request->headers["X-RapidAPI-Host"]);
-        unset($this->request->headers["X-RapidAPI-Key"]);
-    
-    }
-
-    /* Get the user's ID associated with their username. */
-    public function getUserId()
-    {
-
-        $url = "https://medium2.p.rapidapi.com/user/id_for/" . $this->user_name;
-
-        $this->request->get($url);
-
-        $api_response = json_decode($this->request->response, true);
-
-        if (isset($api_response["id"])) {
-
-            $this->user_id = $api_response["id"];
-
-        }
-
-    }
-
-    /* ********************************************************* */
-    /* Functions related to etrieving Articles */
-    /* ********************************************************* */
-
-    /* Getting a user's articles, their info, and their contents */
-    public function getUserArticlesIds()
-    {
-        $url = "https://medium2.p.rapidapi.com/user/" . $this->user_id . "/articles";
-
-        $this->request->get($url);
-
-        $api_response = json_decode($this->request->response, true);
-
-        if (isset($api_response["associated_articles"])) {
-
-            $this->associated_articles_ids = $api_response["associated_articles"];
-
-        }
-
-    }
-
-    /* Get the content (non-html/non-markdown) for a particular article. */
-    public function getUserArticleContent($associated_article_id)
-    {
-
-        $url = "https://medium2.p.rapidapi.com/article/" . $associated_article_id . "/content";
-
-        $this->request->get($url);
-
-        $api_response = json_decode($this->request->response, true);
-
-        if (isset($api_response["content"])) {
-
-            return $api_response["content"];
-
-        }
-
-    }
-
-    /* Get the content (non-html/non-markdown) for all user's articles associated with our object. */
-    public function getUserArticlesContents()
-    {
-
-        foreach ($this->associated_articles_ids as $associated_article_id) {
-
-            $content = $this->getUserArticleContent($associated_article_id);
-
-            $this->associated_articles_contents[] = [
-                "article_id" => $associated_article_id,
-                "content" => $content,
-            ];
-
-        }
-
-    }
-
-    /* Get the content (markdown) for a particular article. */
-    public function getUserArticleMarkdown($associated_article_id)
-    {
-
-        $url = "https://medium2.p.rapidapi.com/article/" . $associated_article_id . "/markdown";
-
-        $this->request->get($url);
-
-        $api_response = json_decode($this->request->response, true);
-
-        if (isset($api_response["markdown"])) {
-
-            return $api_response["markdown"];
-
-        }
-
-    }
-
-    /* Get the content (markdown) for all user's articles associated with our object. */
-    public function getUserArticlesMarkdowns()
-    {
-
-        foreach ($this->associated_articles_ids as $associated_article_id) {
-
-            $markdown = $this->getUserArticleMarkdown($associated_article_id);
-
-            $this->associated_articles_markdowns[] = [
-                "article_id" => $associated_article_id,
-                "markdown" => $markdown,
-            ];
-
-        }
-
-    }
-
-    /* Get the info/metadata for a particular article. */
-    public function getUserArticleInfo($associated_article_id)
-    {
-
-        $url = "https://medium2.p.rapidapi.com/article/" . $associated_article_id;
-
-        $this->request->get($url);
-
-        $api_response = json_decode($this->request->response, true);
-
-        return $api_response;
-
-    }
-
-    /* Get the info/metadata for all user's articles associated with our object. */
-    public function getUserArticlesInfos()
-    {
-
-        foreach ($this->associated_articles_ids as $associated_article_id) {
-
-            $info = $this->getUserArticleInfo($associated_article_id);
-
-            $this->associated_articles_infos[] = [
-                "article_id" => $associated_article_id,
-                "info" => $info,
-            ];
-
-        }
-
-    }
-
-    /* Get the content, markdown, and metadata for all of our user's article associated with our object,
-    and file it away nicely, in memory. 
-    */
-    public function getUserArticles()
-    {
-
-        $this->getUserId();
-
-        $this->getUserArticlesIds();
-
-        foreach ($this->associated_articles_ids as $associated_article_id) {
-
-            $info = $this->getUserArticleInfo($associated_article_id);
-            $markdown = $this->getUserArticleMarkdown($associated_article_id);
-            $content = $this->getUserArticleContent($associated_article_id);
-
-            $this->associated_articles[] = [
-                "article_id" => $associated_article_id,
-                "info" => $info,
-                "markdown" => $markdown,
-                "content" => $content
-            ];
-
-        }
-
-    }
-
-    /* ********************************************************* */
-    /* Functions related to etrieving Articles */
-    /* ********************************************************* */
-
     /* DUmp user's articles for debugging purposes. */
     public function showUserArticles()
     {
